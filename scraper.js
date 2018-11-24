c2e04c38171078f00db7c49ebe819d62ae69edc5
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-11-24 12:24:20 -0700

    Add complete branch

--- scraper.js
+++ scraper.js
@@ -4,8 +4,27 @@ function findHedgieImage(keyword) {
   var nightmare = Nightmare();
 
   return nightmare
-    .goto('')
-    
+    .goto('https://www.google.com')
+    .insert('input[title="Search"]', `hedgehog ${keyword}`)
+    .click('input[value="Google Search"]')
+    .wait('a.q.qs')
+    .click('a.q.qs')
+    .wait('div#res.med')
+    .evaluate(function() {
+      var photoDivs = document.querySelectorAll('img.rg_ic');
+      var list = [].slice.call(photoDivs);
+
+      return list.map(function(div) {
+        return div.src;
+      });
+    })
+    .end()
+    .then(function (result) {
+      return result.slice(1, 5);
+    })
+    .catch(function (error) {
+      console.error('Search failed:', error);
+    });
 }
 
 module.exports = findHedgieImage;
